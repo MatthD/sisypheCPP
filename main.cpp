eb5feb49b07d4656a16b686878615f54268fc352
MatthD <matthias.dieudonne@gmail.com>
2018-02-05 17:32:20 +0100

    Can now write jsonObject in stream file

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
  * */
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <boost/filesystem.hpp>
+#include <json/writer.h>
 
 #include "Magic.h"
-
 
 namespace fs = boost::filesystem;
 using namespace std;
@@ -14,10 +15,14 @@ using namespace std;
 int main(int nbOfArgs, char *args[]) {
   Magic magicInstance{};
   const fs::path p = args[1];
+  ofstream fout("output.json");
   for (const auto& currPath : fs::recursive_directory_iterator(p)){
+    Json::Value file;
     string pathStr = currPath.path().string();
     if(fs::is_regular_file(pathStr)){
-      cout << currPath << " : " << magicInstance.detectMimetype(pathStr) << endl;
+      file["path"] = pathStr;
+      file["mimetype"] = magicInstance.detectMimetype(pathStr);
+      fout << file << endl;
     }
   }
   magicInstance.destroyMagic();
